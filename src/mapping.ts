692b337f28cdc47aabe0f71e3b53e5bdcf5822cb
Taner Topaç <tanertopac@gmail.com>
2020-09-08 16:57:51 +0300

    extra mappers

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,2 +1,6 @@
 import { isObject } from '.';
 export const mapById = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.id : undefined;
+export const mapByValue = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.value : undefined;
+export const mapByType = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.type : undefined;
+export const mapByName = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.name : undefined;
+export const mapBy = (field: string | number) => (obj: any, index: number, arr: any[]) => (isObject(obj) && (!!field || field === 0)) ? obj[field] : undefined;
