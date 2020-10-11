2d647c041218fa42e93620263463a8d452608e2d
Taner Topaç <tanertopac@gmail.com>
2020-10-11 15:45:38 +0300

    Add JSDoc

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,34 @@
 import { isObject } from '.';
+/**
+ * @summary Provides a mapping function which maps by id field of objects
+ * @param obj Object to Map
+ * @param index Index of the Object in Array
+ * @param arr Array to Map 
+ */
 export const mapById = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.id : undefined;
+/**
+ * @summary Provides a mapping function which maps by value field of objects
+ * @param obj Object to Map
+ * @param index Index of the Object in Array
+ * @param arr Array to Map 
+ */
 export const mapByValue = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.value : undefined;
+/**
+ * @summary Provides a mapping function which maps by type field of objects
+ * @param obj Object to Map
+ * @param index Index of the Object in Array
+ * @param arr Array to Map 
+ */
 export const mapByType = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.type : undefined;
+/**
+ * @summary Provides a mapping function which maps by name field of objects
+ * @param obj Object to Map
+ * @param index Index of the Object in Array
+ * @param arr Array to Map 
+ */
 export const mapByName = (obj: any, index: number, arr: any[]) => isObject(obj) ? obj.name : undefined;
+/**
+ * @summary Provides a mapping function which maps by the field represented by name parameter
+ * @param field Name of the Field to Map the Objects by
+ */
 export const mapBy = (field: string | number) => (obj: any, index: number, arr: any[]) => (isObject(obj) && (!!field || field === 0)) ? obj[field] : undefined;
--- src/reducing.ts
+++ src/reducing.ts
@@ -17,8 +17,17 @@ export function sum(...args: number[]): number {
   return result;
 }
 
+/**
+ * @summary Provides an Aggregating Function to Reduce the Array by summing
+ * @param cur Current Value of the Aggregate
+ * @param next Next Value to Handle
+ */
 export const summing: (cur: number, next: number, index: number, array: number[]) => number =  (cur: number, next: number) => sum(cur, next);
 
+/**
+ * @summary Provides an Aggregating Function to Reduce the Array by Summing by the Field represented by name parameter
+ * @param field Name of the Field to sum by
+ */
 export function summingBy<T extends object, K extends keyof T>(field: K): (cur: number, next: T, index: number, array:T[]) => number {
   if(!field) throw new Error('Field is required');
   return (cur: number, next: T) => {
